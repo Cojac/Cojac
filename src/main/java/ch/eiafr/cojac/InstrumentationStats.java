971e4d6bd827b1726ece3fd5baa914cff661b1ee
frederic.bapst <frederic.bapst@hefr.ch>
2013-10-21 20:48:36 +0200

    cosmetic

--- src/main/java/ch/eiafr/cojac/InstrumentationStats.java
+++ src/main/java/ch/eiafr/cojac/InstrumentationStats.java
@@ -24,7 +24,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
