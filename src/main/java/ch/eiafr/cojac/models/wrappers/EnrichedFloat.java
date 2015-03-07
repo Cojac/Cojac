f379fdb9706c92d998b0d6c3cfe7fc207469501f
Sylvain Julmy <sylvain.julmy@edu.hefr.ch>
2015-03-07 20:31:52 +0100

    Just create the class and the overrided method

---
+++ src/main/java/ch/eiafr/cojac/models/wrappers/EnrichedFloat.java
@@ -0,0 +1,36 @@
+package ch.eiafr.cojac.models.wrappers;
+
+import com.kenai.jinterval.rational_bounds.RealInterval;
+
+/**
+ * Created by Snipy on 07.03.15.
+ */
+public class EnrichedFloat extends Number
+{
+    private float value;
+    private RealInterval interval;
+
+    @Override
+    public int intValue()
+    {
+        return (int)value;
+    }
+
+    @Override
+    public long longValue()
+    {
+        return (long)value;
+    }
+
+    @Override
+    public float floatValue()
+    {
+        return value;
+    }
+
+    @Override
+    public double doubleValue()
+    {
+        return (double)value;
+    }
+}
