04d80837d4c48bbd5a80c3188c36e336ed1ec542
frederic.bapst <frederic.bapst@hefr.ch>
2015-11-17 13:24:33 +0100

    small changes in examples/demo

--- src/examples/demo/HelloPolynomial1.java
+++ src/examples/demo/HelloPolynomial1.java
@@ -9,27 +9,27 @@
 package demo;
 
 public class HelloPolynomial1 {
-  
-  static double pow(double base, int exp) {
-    double r=1.0;
-    while(exp-- > 0) r*=base;
-    return r;
-  }
-  
-  public static double somePolynomial(double x, double y) {
-    return 1335.0*(pow(y, 6))/4.0 
-         + x*x*(11.0*x*x*y*y -pow(y, 6) -121.0*pow(y, 4) -2.0)
-         + 11.0*pow(y, 8)/2.0 
-         + x/(2.0*y);
-  }
 
-  public static void main(String[] args) {
-    double r, x, y;
-    x=2.0; y=3.0;
-    r=somePolynomial(x, y);
-    System.out.println("f("+x+" , "+y+") = " + r);
-    x=77617; y=33096;
-    r=somePolynomial(x, y);
-    System.out.println("f("+x+" , "+y+") = " + r);
-  }
-  }
+    static double pow(double base, int exp) {
+        double r=1.0;
+        while(exp-- > 0) r*=base;
+        return r;
+    }
+
+    public static double somePolynomial(double x, double y) {
+        return 1335.0*(pow(y, 6))/4.0 
+                + x*x*(11.0*x*x*y*y -pow(y, 6) -121.0*pow(y, 4) -2.0)
+                + 11.0*pow(y, 8)/2.0 
+                + x/(2.0*y);
+    }
+
+    public static void main(String[] args) {
+        double r, x, y;
+        x=2.0; y=3.0;
+        r=somePolynomial(x, y);
+        System.out.println("f("+x+" , "+y+") = " + r);
+        x=77617; y=33096;
+        r=somePolynomial(x, y);
+        System.out.println("f("+x+" , "+y+") = " + r);
+    }
+}
--- src/examples/demo/HelloPolynomial2.java
+++ src/examples/demo/HelloPolynomial2.java
@@ -7,30 +7,35 @@
 package demo;
 
 public class HelloPolynomial2 {
-  
-  static double pow(double base, int exp) {
-    double r=1.0;
-    while(exp-- > 0) r*=base;
-    return r;
-  }
-  
-  public static double somePolynomial(double x, double y) {
-    return 1335.0*(pow(y, 6))/4.0 
-         + x*x*(11.0*x*x*y*y -pow(y, 6) -121.0*pow(y, 4) -2.0)
-         + 11.0*pow(y, 8)/2.0 
-         + x/(2.0*y);
-  }
-  
-  public static double COJAC_MAGIC_getDerivation(double a)      { return 0; }
-  public static double COJAC_MAGIC_asDerivationTarget(double a) { return a; }
 
-  public static void main(String[] args) {
-    double r, x, y;
-    x=2.0; y=3.0;
-    x=COJAC_MAGIC_asDerivationTarget(x);
-    r=somePolynomial(x, y);
-    System.out.println("f (x,y): "+r);
-    System.out.println("f'(x,y): "+COJAC_MAGIC_getDerivation(r));
-  }
-  
+    static double pow(double base, int exp) {
+        double r=1.0;
+        while(exp-- > 0) r*=base;
+        return r;
+    }
+
+    public static double somePolynomial(double x, double y) {
+        return 1335.0*(pow(y, 6))/4.0 
+                + x*x*(11.0*x*x*y*y -pow(y, 6) -121.0*pow(y, 4) -2.0)
+                + 11.0*pow(y, 8)/2.0 
+                + x/(2.0*y);
+    }
+
+    public static double COJAC_MAGIC_getDerivation(double a)      { return 0; }
+    public static double COJAC_MAGIC_asDerivationTarget(double a) { return a; }
+
+    public static void main(String[] args) {
+        double r, x, y;
+        x=2.0; y=3.0;
+        x=COJAC_MAGIC_asDerivationTarget(x);
+        r=somePolynomial(x, y);
+        System.out.println("f (x,y): "+r);
+        System.out.println("f'(x,y): "+COJAC_MAGIC_getDerivation(r));
+        
+        x=2.0; y=3.0;
+        y=COJAC_MAGIC_asDerivationTarget(y);
+        r=somePolynomial(x, y);
+        System.out.println("f (x,y): "+r);
+        System.out.println("f'(x,y): "+COJAC_MAGIC_getDerivation(r));
+    }
 }
