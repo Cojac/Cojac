dc43094157f912a21a6078fe6c7f6ec4774513aa
frederic.bapst <frederic.bapst@hefr.ch>
2016-03-23 19:19:09 +0100

    comments

--- src/test/java/defects/Buggy02.java
+++ src/test/java/defects/Buggy02.java
@@ -1,5 +1,7 @@
 package defects;
 
+import javax.management.RuntimeErrorException;
+
 import javafx.concurrent.Task;
 
 // doesn't work with Wrapping
@@ -7,12 +9,14 @@ import javafx.concurrent.Task;
 public class Buggy02 {
 
     static class MyTask extends Task<Long> {
+        MyTask() {try {call(); } catch(Exception e) {throw new RuntimeException(e);}}
         @Override protected Long call() throws Exception {
+            updateProgress(0.5, 0.5);  // won't work: inherited from java library
             return 1L;
         }
     }
     
-    public static void main(String... args) {
+    public static void main(String... args) throws Exception {
         Task<Long> u=new MyTask();
         u.getProgress();  // should work via Proxy
 
