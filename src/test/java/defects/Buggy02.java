2c2fb2fd200cc4117d65249b518e6140840a114f
frederic.bapst <frederic.bapst@hefr.ch>
2016-03-24 07:32:06 +0100

    cosmetic

--- src/test/java/defects/Buggy02.java
+++ src/test/java/defects/Buggy02.java
@@ -7,18 +7,17 @@ import javafx.concurrent.Task;
 public class Buggy02 {
 
     static class MyTask extends Task<Long> {
-        MyTask() {try {call(); } catch(Exception e) {throw new RuntimeException(e);}}
+//        MyTask() {try {call(); } catch(Exception e) {throw new RuntimeException(e);}}
         @Override protected Long call() throws Exception {
-            updateProgress(0.5, 0.5);  // won't work: inherited from java library
+//            updateProgress(0.5, 0.5);  // won't work: inherited from java library
             return 1L;
         }
     }
     
     public static void main(String... args) throws Exception {
-        Task<Long> u=new MyTask();
-        u.getProgress();  // should work via Proxy
-
         MyTask t=new MyTask();
+        Task<Long> u=t;
+        u.getProgress();  // should work via Proxy
         t.getProgress();  // won't work: inherited from java library
     }
 }
