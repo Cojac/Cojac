385d012b4bcc2c942f82617d0e219ea4aefbfad6
valentin.gazzola <valentin.gazzola@edu.hefr.ch>
2016-04-01 11:34:59 +0200

    initial <<conversion behaviour>>, the idea doesn't satisfies me as such.

---
+++ src/main/java/com/github/cojac/models/ConversionBehaviour.java
@@ -0,0 +1,134 @@
+package com.github.cojac.models;
+
+public interface ConversionBehaviour {
+   
+
+    public static double DADD(double a, double b) {
+        return outTransform(inTransform(a) + inTransform(b));
+    }
+    public static double DSUB(double a, double b) {
+        return outTransform(inTransform(a)- inTransform(b));
+    }
+    public static double DMUL(double a, double b) {
+        return outTransform(inTransform(a) * inTransform(b));
+    }
+    public static double DDIV(double a, double b){
+        return outTransform(inTransform(a) / inTransform(b));
+    }
+    public static double DREM(double a, double b){
+        return outTransform(inTransform(a) %inTransform(b));
+    }
+    public static int DCMPL(double a, double b){
+        return Double.compare(inTransform(a) ,inTransform(b));
+    }
+    public static int DCMPG(double a, double b){
+        if(Double.isNaN(a)||Double.isNaN(b))
+            return 1;
+        return DCMPL(a,b);
+    }
+    /*public static double abs(double a){
+        return outTransform( Math.abs(a)));
+    }
+    public static double acos(double a){
+        return outTransform( Math.acos(inTransform(a)));
+    }
+    public static double asin(double a){
+        return outTransform( Math.asin(inTransform(a)));
+    }
+    public static double atan(double a){
+        return outTransform( Math.atan(inTransform(a)));
+    }
+    public static double atan2(double x,double y){
+        return outTransform( Math.atan2(inTransform(x,inTransform(y)));
+    }
+    public static double cbrt(double a){
+        return outTransform( Math.cbrt(inTransform(a)));
+    }
+    public static double copySign(double a,double b){
+        return Math.copySign(inTransform(a,inTransform(b)));
+    }
+    public static double cos(double a){
+        return outTransform( Math.cos(inTransform(a)));
+    }
+    public static double cosh(double a){
+        return outTransform( Math.cosh(inTransform(a)));
+    }
+    public static double exp(double a){
+        return outTransform( Math.exp(inTransform(a)));
+    }
+    public static double expm1(double a){
+        return outTransform( Math.expm1(inTransform(a)));
+    }
+    public static int getExponent(double a){
+        return Math.getExponent(inTransform(a)));
+    }
+    public static double hypot(double a,double b){
+        return outTransform( Math.hypot(inTransform(a,inTransform(b)));
+    }
+    public static double IEEEremainder(double a,double b){
+        return outTransform( Math.IEEEremainder(inTransform(a,inTransform(b)));
+    }
+    public static double log(double a){
+        return outTransform( Math.log(inTransform(a)));
+    }
+    public static double log10(double a){
+        return outTransform( Math.log10(inTransform(a)));
+    }
+    public static double log1p(double a){
+        return outTransform( Math.log1p(inTransform(a)));
+    }
+    public static double max(double a, double b){
+        return Math.max(inTransform(a, inTransform(b)));
+    }
+    public static double min(double a, double b){
+        return Math.min(inTransform(a, inTransform(b)));
+    }
+    public static double nextAfter(double a, double b){
+        return Math.nextAfter(inTransform(a, inTransform(b)));
+    }
+    public static double nextDown(double a){
+        return  Math.nextDown(inTransform(a)));
+    }
+    public static double nextUp(double a){
+        return  Math.nextUp(inTransform(a));
+    }
+    public static double pow(double a, double b){
+        return outTransform( Math.pow(inTransform(a), inTransform(b)));
+    }*/
+    public static double random(){
+        return outTransform( Math.random());
+    }
+    public static double scalb(double a, int b){
+        return outTransform(Math.scalb(inTransform( a),b));
+    }
+    public static double sin(double a){
+        return outTransform( Math.sin(inTransform(a)));
+    }
+    public static double sinh(double a){
+        return outTransform( Math.sinh(inTransform(a)));
+    }
+    public static double sqrt(double a){
+        return outTransform( Math.sqrt(inTransform(a)));
+    }
+    public static double tan(double a){
+        return outTransform( Math.tan(inTransform(a)));
+    }
+    public static double tanh(double a){
+        return outTransform( Math.tanh(inTransform(a)));
+    }
+    public static double toDegrees(double a){
+        return outTransform( Math.toDegrees(inTransform(a)));
+    }
+    public static double toRadians(double a){
+        return outTransform( Math.toRadians(inTransform(a)));
+    }
+    public static double ulp(double a){
+        return Math.ulp(inTransform(a));
+    }
+    static double inTransform(double a){
+        return a;
+    }
+    static double outTransform(double a){
+        return a;
+    }
+}
