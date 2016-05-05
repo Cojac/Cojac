1f515d4c043a9c441682fecf33b3775f57d434b6
valentin.gazzola <valentin.gazzola@edu.hefr.ch>
2016-05-05 09:54:58 +0200

    added methods for transforming variables

--- src/main/java/com/github/cojac/models/ConstTransform.java
+++ src/main/java/com/github/cojac/models/ConstTransform.java
@@ -1,10 +1,17 @@
 package com.github.cojac.models;
 
 import org.objectweb.asm.Opcodes;
-
+import static com.github.cojac.models.Signatures.*;
+import static com.github.cojac.models.Parameters.*;
 public enum ConstTransform {
-    doubleTransform(new Operation(Opcodes.INVOKESTATIC,"fromUninstrumentedDouble", "(D)D", Parameters.DOUBLE_BINARY_PARAMS.params), double.class);
-    
+    doubleTransform(new Operation(Opcodes.INVOKESTATIC,"fromUninstrumentedDouble", DOUBLE_UNARY.description,
+            DOUBLE_BINARY_PARAMS.params), double.class),
+    floatTransform(new Operation(Opcodes.INVOKESTATIC,"fromUninstrumentedFloat", FLOAT_UNARY.description,
+            FLOAT_BINARY_PARAMS.params), float.class),
+    intTransform(new Operation(Opcodes.INVOKESTATIC,"fromUninstrumentedInt", INTEGER_UNARY.description,
+            INTEGER_BINARY_PARAMS.params), int.class),
+    longTransform(new Operation(Opcodes.INVOKESTATIC,"fromUninstrumentedLong", LONG_UNARY.description,
+            LONG_BINARY_PARAMS.params), long.class);
     public final Operation operation;
     public final Class<?> constType;
     private ConstTransform(Operation operation, Class<?> constType) {
