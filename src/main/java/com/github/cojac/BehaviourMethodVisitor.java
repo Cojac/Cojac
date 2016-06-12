fe5ecaeb62e014f45ec8a85c542668f3d970d2eb
remi.badoud <remi.badoud@edu.hefr.ch>
2016-06-12 19:16:42 +0200

    Extension de l'instrumentation : il est maintenant possible d'instrumenter en fonction d'un fichier XML (contenant la liste des instructions que l'on désire instrumenter)

--- src/main/java/com/github/cojac/BehaviourMethodVisitor.java
+++ src/main/java/com/github/cojac/BehaviourMethodVisitor.java
@@ -26,6 +26,7 @@ import org.objectweb.asm.commons.LocalVariablesSorter;
 import com.github.cojac.instrumenters.IOpcodeInstrumenter;
 import com.github.cojac.instrumenters.BehaviourInstrumenter;
 import com.github.cojac.models.Operations;
+import com.github.cojac.utils.BehaviourLoader;
 import com.github.cojac.utils.InstructionWriter;
 import com.github.cojac.instrumenters.IOpcodeInstrumenterFactory;
 
@@ -36,12 +37,14 @@ import com.github.cojac.instrumenters.IOpcodeInstrumenterFactory;
  */
 
 import java.util.BitSet;
+
 final class BehaviourMethodVisitor extends LocalVariablesSorter {
     private final IOpcodeInstrumenterFactory factory;
-    private static BitSet constLoadInst = new BitSet(255);//there is 255 possible opcodes
-    static{
-        for(Operations op: Operations.values()){
-            if(op.loadsConst)
+    private static BitSet constLoadInst = new BitSet(255);// there is 255
+                                                          // possible opcodes
+    static {
+        for (Operations op : Operations.values()) {
+            if (op.loadsConst)
                 constLoadInst.set(op.opCodeVal);
         }
     }
@@ -50,11 +53,11 @@ final class BehaviourMethodVisitor extends LocalVariablesSorter {
     private final String classPath;
     private final CojacReferences references;
     private boolean instrumentMethod = false;
-    private int lineNb=0;
+    private int lineNb = 0;
     private BehaviourClassVisitor bcv;
-    BehaviourInstrumenter instrumenter ;
-    BehaviourMethodVisitor(int access, String desc, MethodVisitor mv, InstrumentationStats stats, Args args, String classPath,
-            IOpcodeInstrumenterFactory factory, CojacReferences references, String MethodName, BehaviourClassVisitor behaviourClassVisitor) {
+    BehaviourInstrumenter instrumenter;
+
+    BehaviourMethodVisitor(int access, String desc, MethodVisitor mv, InstrumentationStats stats, Args args, String classPath, IOpcodeInstrumenterFactory factory, CojacReferences references, String MethodName, BehaviourClassVisitor behaviourClassVisitor) {
         super(Opcodes.ASM5, access, desc, mv);
 
         this.stats = stats;
@@ -63,7 +66,8 @@ final class BehaviourMethodVisitor extends LocalVariablesSorter {
 
         this.classPath = classPath;
         this.references = references;
-        instrumentMethod = references.hasToBeInstrumented(classPath, MethodName+desc);
+        instrumentMethod = references.hasToBeInstrumented(classPath, MethodName +
+                desc);
         instrumenter = BehaviourInstrumenter.getInstance(args, stats);
         this.bcv = behaviourClassVisitor;
     }
@@ -73,63 +77,119 @@ final class BehaviourMethodVisitor extends LocalVariablesSorter {
         System.out.println("BehaviourMethodVisitor.visitInsn()");
         bcv.incInstructionCounter();
         int instructionNb = bcv.getInstructionCounter();
-        //TODO : condition
-        if(args.isSpecified(Arg.LISTING_INSTRUCTIONS))
+        if (args.isSpecified(Arg.LISTING_INSTRUCTIONS))
             InstructionWriter.getinstance().appendInstruction(classPath, lineNb, instructionNb, opCode);
         IOpcodeInstrumenter instrumenter = factory.getInstrumenter(opCode);
-        //System.out.println("Has to be instrumented: "+references.hasToBeInstrumented(classPath, lineNb, instructionNb));
-        //Delegate to parent
-        if ((references.hasToBeInstrumented(classPath, lineNb, instructionNb, opCode)||instrumentMethod) &&instrumenter != null ) {
-            if(constLoadInst.get(opCode)){//the operation is a constant loading one
-                super.visitInsn(opCode);//load the constant
-                visitConstantLoading(Operations.getReturnType(opCode));//transform it
-            }else{
-                instrumenter.instrument(mv, opCode); //, classPath, methods, reaction, this);
+
+        if (args.isSpecified(Arg.LOAD_BEHAVIOUR_MAP)) {
+            if (BehaviourLoader.getinstance().contains(classPath, lineNb, instructionNb, opCode) &&
+                    instrumenter != null) {
+                if (constLoadInst.get(opCode)) {// the operation is a constant
+                                                // loading one
+                    super.visitInsn(opCode);// load the constant
+                    visitConstantLoading(Operations.getReturnType(opCode));// transform
+                                                                           // it
+                } else {
+                    instrumenter.instrument(mv, opCode); // , classPath,
+                                                         // methods, reaction,
+                                                         // this);
+                }
+            } else {
+                super.visitInsn(opCode);
             }
-        } else{
-            super.visitInsn(opCode);
+        } // System.out.println("Has to be instrumented:
+          // "+references.hasToBeInstrumented(classPath, lineNb,
+          // instructionNb));
+          // Delegate to parent
+        else {
+            if ((references.hasToBeInstrumented(classPath, lineNb, instructionNb, opCode) ||
+                    instrumentMethod) && instrumenter != null) {
+                if (constLoadInst.get(opCode)) {// the operation is a constant
+                                                // loading one
+                    super.visitInsn(opCode);// load the constant
+                    visitConstantLoading(Operations.getReturnType(opCode));// transform
+                                                                           // it
+                } else {
+                    instrumenter.instrument(mv, opCode); // , classPath,
+                                                         // methods, reaction,
+                                                         // this);
+                }
+            } else {
+                super.visitInsn(opCode);
+            }
         }
     }
+
     @Override
     public void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itf) {
         System.out.println("BehaviourMethodVisitor.visitMethodInsn()");
         bcv.incInstructionCounter();
-        
+
         int instructionNb = bcv.getInstructionCounter();
-        if(args.isSpecified(Arg.LISTING_INSTRUCTIONS))
+        if (args.isSpecified(Arg.LISTING_INSTRUCTIONS))
             InstructionWriter.getinstance().appendInstruction(classPath, lineNb, instructionNb, opcode);
-        //System.out.println("Has to be instrumented: "+references.hasToBeInstrumented(classPath, lineNb, instructionNb));
-        if ((references.hasToBeInstrumented(classPath, lineNb, instructionNb, opcode)||instrumentMethod) && instrumenter.wantsToInstrumentMethod(opcode, owner,name,desc)){
-            instrumenter.instrumentMethod(mv,owner, name, desc);
-        }else{
-            super.visitMethodInsn(opcode, owner, name, desc, itf);
+        // System.out.println("Has to be instrumented:
+        // "+references.hasToBeInstrumented(classPath, lineNb, instructionNb));
+
+        if (args.isSpecified(Arg.LOAD_BEHAVIOUR_MAP)) {
+            if (BehaviourLoader.getinstance().contains(classPath, lineNb, instructionNb, opcode) &&
+                    instrumenter.wantsToInstrumentMethod(opcode, owner, name, desc)) {
+                instrumenter.instrumentMethod(mv, owner, name, desc);
+            } else {
+                super.visitMethodInsn(opcode, owner, name, desc, itf);
+            }
+        } else {
+            if ((references.hasToBeInstrumented(classPath, lineNb, instructionNb, opcode) ||
+                    instrumentMethod) &&
+                    instrumenter.wantsToInstrumentMethod(opcode, owner, name, desc)) {
+                instrumenter.instrumentMethod(mv, owner, name, desc);
+            } else {
+                super.visitMethodInsn(opcode, owner, name, desc, itf);
+            }
         }
-   
+
     }
+
     @Override
-    public void visitLdcInsn(Object cst){
+    public void visitLdcInsn(Object cst) {
         System.out.println("BehaviourMethodVisitor.visitLdcInsn()");
         bcv.incInstructionCounter();
         int instructionNb = bcv.getInstructionCounter();
-        if(args.isSpecified(Arg.LISTING_INSTRUCTIONS))
+        if (args.isSpecified(Arg.LISTING_INSTRUCTIONS))
             InstructionWriter.getinstance().appendInstruction(classPath, lineNb, instructionNb, Opcodes.LDC);
         super.visitLdcInsn(cst);
-        //System.out.println("Has to be instrumented: "+references.hasToBeInstrumented(classPath, lineNb, instructionNb));
-        if((references.hasToBeInstrumented(classPath, lineNb, instructionNb, Opcodes.LDC)||instrumentMethod)&&instrumenter.wantsToInstrumentConstLoading(cst.getClass())){
-            //instrumenter.instrumentLDC(mv, cst);
-            visitConstantLoading(cst.getClass());
-        }else{
-         //   super.visitLdcInsn(cst);
+        if (args.isSpecified(Arg.LOAD_BEHAVIOUR_MAP)) {
+            if (BehaviourLoader.getinstance().contains(classPath, lineNb, instructionNb, Opcodes.LDC) &&
+                    instrumenter.wantsToInstrumentConstLoading(cst.getClass())) {
+                // instrumenter.instrumentLDC(mv, cst);
+                visitConstantLoading(cst.getClass());
+            } else {
+                // super.visitLdcInsn(cst);
+            }
+        }
+        // System.out.println("Has to be instrumented:
+        // "+references.hasToBeInstrumented(classPath, lineNb, instructionNb));
+        else {
+            if ((references.hasToBeInstrumented(classPath, lineNb, instructionNb, Opcodes.LDC) ||
+                    instrumentMethod) &&
+                    instrumenter.wantsToInstrumentConstLoading(cst.getClass())) {
+                // instrumenter.instrumentLDC(mv, cst);
+                visitConstantLoading(cst.getClass());
+            } else {
+                // super.visitLdcInsn(cst);
+            }
         }
     }
-    private void visitConstantLoading(Class<?> cl){
+
+    private void visitConstantLoading(Class<?> cl) {
         System.out.println("BehaviourMethodVisitor.visitConstantLoading()");
         instrumenter.instrumentConstLoading(mv, cl);
     }
+
     @Override
-    public void visitLineNumber(int line, Label start){
+    public void visitLineNumber(int line, Label start) {
         System.out.println("BehaviourMethodVisitor.visitLineNumber()");
-        lineNb = line; 
+        lineNb = line;
         super.visitLineNumber(line, start);
     }
 }
