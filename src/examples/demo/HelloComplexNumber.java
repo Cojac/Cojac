f6018873fba213a0081954bec45c21ef667c27fa
frederic.bapst <frederic.bapst@hefr.ch>
2021-08-02 19:18:38 +0200

    improve HelloComplex demo

--- src/examples/demo/HelloComplexNumber.java
+++ src/examples/demo/HelloComplexNumber.java
@@ -21,13 +21,69 @@
 
 package demo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class HelloComplexNumber {
+    static double conjugate(double a) {
+        double module = Math.abs(a);
+        return module * module / a;
+    }
+
+    static double realPartOnly(double a) {
+        return (a + conjugate(a))/2;
+    }
+
+    static double imaginaryPartOnly(double a) {
+        return a - realPartOnly(a);
+    }
+
+    static double imaginaryCoefficient(double a) {
+        return imaginaryPartOnly(a) * (-Math.sqrt(-1));
+    }
 
     static void tinyDemo() {
-        double val = Math.sqrt(-9); // = 3i
-        val = 5 * val * val;        // = 5*9*i^2 = -45
-        System.out.print(val);    // -45.0 (with COJAC!)
-        System.out.println(" should be -45.0 (with COJAC!)");
+        double v = Math.sqrt(-9); // = 3i
+        double val = 5 * v * v;     // = 5*9*i^2 = -45
+        System.out.println(val + " …should be '-45.0' (with COJAC!)");
+        double y = 4.0 + v;
+        System.out.println(y + " …should be '4 + 3i'");
+        System.out.println(" conjugate: "+ conjugate(y) + " …should be '4 - 3i'");
+        System.out.println("  realPart: "+ realPartOnly(y) + " …should be '4'");
+        System.out.println("   imgPart: "+ imaginaryPartOnly(y) + " …hould be '3i'");
+        System.out.println("   imgCoef: "+ imaginaryCoefficient(y) + " …should be '3'");
+    }
+
+
+    // Find a root of a cubic equation of the form ax^3 + bx^2 + cx + d = 0 with the general cubic formula
+    // This formula can be found on wikipedia: https://en.wikipedia.org/wiki/Cubic_equation#General_cubic_formula
+    static List<Double> cubicEquationSolutions(double a, double b, double c, double d) {
+        List<Double> sols = new ArrayList<>();
+        double det0 = b * b - 3 * a * c;
+        double det1 = 2 * b * b * b - 9 * a * b * c + 27 * a * a * d;
+
+        double sqrt = Math.sqrt(det1 * det1 - 4 * det0 * det0 * det0);
+        if (Double.isNaN(sqrt))
+            // the root can't be calculated if this value is not available.
+            return sols;
+        double coef = Math.cbrt((det1 - sqrt) / 2);
+        if (coef == 0) {
+            coef = Math.cbrt((det1 + sqrt) / 2);
+        }
+        double coef2 = coef * (-1 + Math.sqrt(-3)) / 2;
+        double coef3 = coef * (-1 - Math.sqrt(-3)) / 2;
+        if (coef == 0) {
+            sols.add(-b / (3 * a));
+            return sols;
+        }
+        double res1 = -(b + coef + det0 / coef) / (3 * a);
+        double res2 = -(b + coef2 + det0 / coef2) / (3 * a);
+        double res3 = -(b + coef3 + det0 / coef3) / (3 * a);
+        sols.add(res1);
+        sols.add(res2);
+        sols.add(res3);
+        return sols;
     }
 
     // Find a root of a cubic equation of the form ax^3 + bx^2 + cx + d = 0 with the general cubic formula
@@ -53,7 +109,12 @@ public class HelloComplexNumber {
 
     public static void main(String[] args) {
         tinyDemo();
-        System.out.println(solveCubicEquation(2, 1, 3, 1) + " should be ≈ -0.34563");
-        System.out.println(solveCubicEquation(1, -2, -13, -10) + " should be -1, -2 or 5");
+        List<Double> sols;
+        sols = cubicEquationSolutions(2, 1, 3, 1);
+        String expected = "{-0.34563, -0.07719-1.20029i, -0.07719+1.20029i}";
+        System.out.println(sols + "\n …should be " + expected);
+        sols = cubicEquationSolutions(1, -2, -13, -10);
+        expected = "{-2, -1, -5}";
+        System.out.println(sols + "\n …should be " + expected);
     }
 }
